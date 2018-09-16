e48304648570080787fa357b6d77c7f66aff6bb3
Tobias Valbjørn <tvalbjoern@gmail.com>
2018-09-16 16:41:11 +0200

    Clean up and comments

--- hellomkrfox.ino
+++ hellomkrfox.ino
@@ -3,47 +3,26 @@
 #include <RTCZero.h>
 #include <ArduinoLowPower.h>
 
-
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);      // open the serial port at 9600 bps 
+  // open the serial port at 9600 bps
+  Serial.begin(9600);       
 
+  // A special way to handle serial communication with MKRFox1200. 
+  // Since the MKRFox1200 doesn't have an autoreset function 
+  // associated to the Serial monitor opening, a Serial while is present
   while(!Serial) {};
   if (!SigFox.begin()) {
+    // We also check that the SigFox module is present and can be used.
     Serial.println("Shield error or not present!");
     return;
 }
- String version = SigFox.SigVersion();
+  //Get the necessary device information for activation on Sigfox. 
+  String version = SigFox.SigVersion();
   String ID = SigFox.ID();
   String PAC = SigFox.PAC();
 
-  // Display module informations
+  // Display module informations on serial monitor
   Serial.println("MKRFox1200 Sigfox first configuration");
   Serial.println("SigFox FW version " + version);
   Serial.println("ID  = " + ID);
@@ -54,10 +33,6 @@ void setup() {
   // Send the module to the deepest sleep
   SigFox.end();
 }
-
 // the loop function runs over and over again forever
-void loop() {
-
- 
-                      
+void loop() {                     
 }

0ec8cf62fb33840417d3b748ecdbce2b5cd91b8a
tobiasvalbjoern <tvalbjoern@gmail.com>
2018-11-07 20:25:50 +0100

    Enabled internal pull ups on unused GPIO pins

--- trigger_event/trigger_event.ino
+++ trigger_event/trigger_event.ino
@@ -1,3 +1,10 @@
+//Source file   : trigger_event.ino
+//Author        : Tobias Valbjørn
+//Date          : 15-oct-2018
+//Version       : 1.3
+//Description   : 
+//The program checks to see if Pin0 is "0". If it is, it will send a sigfox 
+//packet with the text "ALM", the id of the device as well as a code for the alarm source
 #include <SigFox.h>
 
 //pin for trigger
@@ -6,22 +13,26 @@ int trig_pin = 0;
 //assume that power is on as initial condition
 int state_trig = HIGH;
 int state_trig_old = HIGH;
-
 int alarm_source = 1;
+
+//Set debug to false when it is used on-site. 
+//Set debug to true, when you want to use serial monitor. 
+int debug=false;
 
 void setup() {
 
+  if(debug==true){
   // open the serial port at 9600 bps
   Serial.begin(9600);
-
   // A special way to handle serial communication with MKRFox1200.
   // Since the MKRFox1200 doesn't have an autoreset function
   // associated to the Serial monitor opening, a Serial while is present
   while (!Serial) {};
+  }
 
   if (!SigFox.begin()) {
     //something is really wrong, try rebooting
-    Serial.println("Rebooting...");
+    if(debug==true){Serial.println("Rebooting...");}
     reboot();
   }
 
@@ -31,35 +42,41 @@ void setup() {
   SigFox.reset();
   delay(100);
 
+  if(debug==true){
   //debug needs to be there if using serial.
   SigFox.debug();
-
+  }
   //Send module to standby until we need to send a message
   SigFox.end();
 
 
   // We need to have time to program the Arduino after a reset
   // Otherwise it does not respond when in low-power mode
-  Serial.println("Booting...");
+  if(debug==true){Serial.println("Booting...");}
   digitalWrite(LED_BUILTIN, HIGH);
   delay(5000);
   digitalWrite(LED_BUILTIN, LOW);
 
   //interrupt pin
-  pinMode(trig_pin, INPUT);
-  
+  pinMode(trig_pin, INPUT);  
   //turn on pull-up resistors. The input will stay high, unless the INPUT
   // is logic low.
   digitalWrite(trig_pin, HIGH);
 
+  //Don't leave the unsued GPIO pins floating
+  pinMode(1,INPUT_PULLUP);
+  pinMode(2,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  pinMode(5,INPUT_PULLUP);
+  pinMode(6,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
 }
 
 void loop()
 {
-
   power_off();
   power_on();
-
 }
 
 
@@ -89,7 +106,7 @@ int power_off() {
     // 3 bytes (ALM) + 4 bytes (ID) + 1 byte (source) < 12 bytes
     String to_be_sent = "ALM" + SigFox.ID() +  String(alarm_source);
     
-    Serial.println("Sending " + to_be_sent);
+    if(debug==true){Serial.println("Sending " + to_be_sent);}
 
     //Begins the process of sending a packet
     SigFox.beginPacket();
@@ -103,18 +120,19 @@ int power_off() {
     int ret = SigFox.endPacket();
     
     if (ret > 0) {
-      Serial.println("No transmission");
+      if(debug==true){Serial.println("No transmission");}
       return 0;
     } else {
-      Serial.println("Transmission ok");
+      if(debug==true){Serial.println("Transmission ok");}
       state_trig_old = state_trig;
       delay(100);
     }
+    if(debug==true){
     Serial.println("Sigfox status");
     Serial.println(SigFox.status(SIGFOX));
     Serial.println("Atmel status: ");
     Serial.println(SigFox.status(ATMEL));
-
+    }
     // shut down module, back to standby
     SigFox.end();
   }
